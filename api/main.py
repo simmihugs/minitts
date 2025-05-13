5003212bde526ac035a4ac009a62839052ee660c
simmihugs <simon.graetz@gmx.de>
2025-05-13 18:00:32 +0200

    changed the request model to autogenerate the file name

--- api/main.py
+++ api/main.py
@@ -9,29 +9,30 @@ from api.models.myrequest import MyRequest
 streamer = Streamer()
 app = FastAPI()
 
-origins = [
-    "*"
-]
+origins = ["*"]
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['*'],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/api/data")
 async def get_data():
     return {"message": "data from backend"}
 
+
 @app.post("/stream-audio-static")
 async def stream_audio_static(request: MyRequest):
-    if not os.path.exists(request.output_path):
+    if not os.path.exists(request.output_path()):
         speech_creator = SpeechCreator()
-        speech_creator.create_audio(output_path=request.output_path, text=request.text)
+        speech_creator.create_audio(output_path=request.output_path(), text=request.text)
     return StreamingResponse(
-        streamer.generate_audio_chunks_from_file(request.output_path),
+        streamer.generate_audio_chunks_from_file(request.output_path()),
         media_type="audio/wav",
     )
 
--- api/models/myrequest.py
+++ api/models/myrequest.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
+import hashlib
 
 class MyRequest(BaseModel):
     text: str
-    output_path: str
+
+    def output_path(self) -> str:
+        text_bytes = self.text.encode('utf-8')
+        hashed_text = hashlib.md5(text_bytes).hexdigest()
+        return f"{hashed_text}.wav"
